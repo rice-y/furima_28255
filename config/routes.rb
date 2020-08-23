5372231c8b322be23d387af2d58f7c23290bdace
rice-y <ykmj00873@gmail.com>
2020-08-23 18:45:51 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources:users, only:[:new, :create, :destroy]
-  resources:items, only:[:index, :new, :create, :show]
+  resources:items, only:[:index, :new, :create, :show, :edit]
  
 end
 
