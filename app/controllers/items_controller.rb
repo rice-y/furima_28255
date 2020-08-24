07058b52ad2aea3311c63de5b284bc9f533e425b
rice-y <ykmj00873@gmail.com>
2020-08-24 18:58:04 +0900

    商品削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 before_action :move_to_index, except: [ :index, :show ]
-before_action :show_item, only: [ :show, :edit, :update]
+before_action :show_item, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @items=  Item.all.order("created_at DESC")
@@ -35,6 +35,14 @@ before_action :show_item, only: [ :show, :edit, :update]
    else
     render :edit
    end
+  end
+
+  def destroy
+   if @item.destroy
+    redirect_to root_path
+    else
+    render :show
+    end
   end
 
 
