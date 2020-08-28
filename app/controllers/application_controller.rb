ede63bd0558a88e34f98138f6dfc9a5357fb395b
rice-y <ykmj00873@gmail.com>
2020-08-28 15:33:32 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -10,10 +10,10 @@ class ApplicationController < ActionController::Base
   
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["AUTH_USER"] && password == ENV["AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-    end
+  # def basic_auth
+    # authenticate_or_request_with_http_basic do |username, password|
+      # username == ENV["AUTH_USER"] && password == ENV["AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    # end
 
-  #end
+  # end
 end
