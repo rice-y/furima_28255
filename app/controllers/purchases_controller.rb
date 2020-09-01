13232cffa22235422bbb8410e00f5f53613c4233
rice-y <ykmj00873@gmail.com>
2020-09-01 11:20:01 +0900

    payjpの記述（js.controller）

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,57 @@
 class PurchasesController < ApplicationController 
-  
+  before_action :move_to_index, only: [:index, :create]
   def index
      @item = Item.find(params[:item_id])
   end
 
-  def create
+  def new
+    @purchase =PurchaseAddress.new(purchase_params)
+
   end
+
+
+  def create
+    @purchase =PurchaseAddress.new(purchase_params)
+
+    if @purchase.valid?
+      pay_item       
+      @purchase.save
+      binding.pry
+        redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
+  end
+
+
+  private
+
+  def purchase_params
+    params.permit(:token,:authenticity_token,:price, :item_id, :postal_code,  :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
+
+
+  # def order_params
+  #   params.permit(:token,:price)
+  # end
+
+  def pay_item
+    
+    Payjp.api_key = "PAYJP_SECRET_KEY"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: purchase_params[:price],  # 商品の値段
+      card:   purchase_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+ 
+
   
+
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_XXXXXXXX");　// PAY.JPテスト公開鍵
+  Payjp.setPublicKey("PAYJP_PUBLIC_KEY");// PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,14 +13,14 @@ const pay = () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
-
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-
+        
         document.getElementById("number").removeAttribute("name");
         document.getElementById("cvc").removeAttribute("name");
         document.getElementById("exp_month").removeAttribute("name");
