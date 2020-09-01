f889b54774bda9357ea034bd03687c523e6c5b41
rice-y <ykmj00873@gmail.com>
2020-09-01 17:23:54 +0900

    PR修正2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,9 +13,9 @@ const pay = () => {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
-     console.log(card)
+     
     Payjp.createToken(card, (status, response) => {
-      console.log(status)
+      
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
